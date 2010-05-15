530067643a9cf360c6b909f4d6895b5d506c5f9e
Trek Glowacki <pietrekg@umich.edu>
2010-05-15 12:09:55 -0400

    updated other suite runners to correct folder structure

--- spec/node.js
+++ spec/node.js
@@ -2,9 +2,9 @@
 require.paths.unshift('spec', './spec/lib', 'lib')
 require('jspec')
 require('unit/spec.helper')
-require('yourlib')
+require('mustache')
 
 JSpec
-  .exec('spec/unit/spec.js')
-  .run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures', failuresOnly: true })
+  .exec('spec/unit/parsing.js')
+  .run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/examples', failuresOnly: true })
   .report()
--- spec/rhino.js
+++ spec/rhino.js
@@ -1,10 +1,10 @@
 
 load('./spec/lib/jspec.js')
 load('./spec/lib/jspec.xhr.js')
-load('lib/yourlib.js')
+load('lib/mustache.js')
 load('spec/unit/spec.helper.js')
 
 JSpec
-.exec('spec/unit/spec.js')
-.run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
+.exec('spec/unit/parsing.js')
+.run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/examples' })
 .report()
